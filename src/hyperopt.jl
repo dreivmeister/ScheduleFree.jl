af04f16f61df0c80d3d344d02e94cc544da418a3
dreivmeister <felix.brandt@fh-bielefeld.de>
2025-03-05 17:06:14 +0100

    new test

--- src/hyperopt.jl
+++ src/hyperopt.jl
@@ -6,17 +6,22 @@ function hyperparametersweep(rule::Symbol, f, x0, maxiters, hyperparams)
     param_combinations = collect(Iterators.product(values(hyperparams)...))
     hyperparam_keys = keys(hyperparams)
     named_param_combinations = [Dict(zip(hyperparam_keys,combo)) for combo in param_combinations]
+    
     best_hyperparams = Dict()
     best_params = Inf
     best_params_value = Inf
+
     for params in named_param_combinations
         final_params = optimize(Val(rule), f, x0, maxiters; params...)
         final_params_value = f(final_params)
+
         if final_params_value < best_params_value
             best_params = final_params
             best_params_value = final_params_value
             best_hyperparams = params
         end
+
     end
+
     return best_hyperparams, best_params, best_params_value
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,6 @@ x_opt = optimize(:SF_ADAMW, f, x0, 1000)
 
 best_hyperparams, best_x, best_x_value = hyperparametersweep(:SF_SGD, f, x0, 100, Dict(:γ => [0.1, 0.01], :β => [0.9, 0.99]))
 @assert best_x_value < 1e-3 "SF_SGD hyperparametersweep error is too large: $(best_x_value)"
+
+best_hyperparams, best_x, best_x_value = hyperparametersweep(:SF_ADAMW, f, x0, 100, Dict(:γ => [0.0025, 0.05], :λ => [0, 0.1], :β1 => [0.9], :β2 => [0.999]))
+@assert best_x_value < 1e-1 "SF_ADAMW hyperparametersweep error is too large: $(best_x_value)"
